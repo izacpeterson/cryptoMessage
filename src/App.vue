5053092e9800ea0ac8f9358c51422e690a3ca5fb
izacpeterson <izacpeterson@gmail.com>
2022-03-04 08:06:08 -0700

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import logo from "./assets/logo.svg";
 </script>
 
 <template>
   <header>
-    <RouterLink to="/encrypt">Encrypt</RouterLink>
-    <RouterLink to="/decrypt">Decrypt</RouterLink>
+    <img src="./assets/logo.svg" alt="" />
   </header>
-  <main>
-    <RouterView />
-  </main>
+  <RouterView />
 </template>
 
 <style>
@@ -18,54 +15,17 @@ import HelloWorld from "@/components/HelloWorld.vue";
 header {
   display: flex;
   justify-content: space-evenly;
-  padding: 20px;
-}
-a {
-  color: white;
-  text-decoration: none;
-  padding: 20px;
-}
-a:hover {
-  background-color: rgba(0, 255, 0, 0.205);
-}
-.input {
-  display: flex;
-  flex-direction: column;
-  /* align-items: flex-start; */
   align-items: center;
-  justify-content: center;
+  padding: 20px;
+  margin-bottom: 10px;
 }
-label {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  /* width: 100%; */
+img {
+  width: 75px;
 }
 main {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
-button {
-  margin: 20px;
-  padding: 10px;
-  background-color: rgb(63, 160, 63);
-  color: white;
-  border: none;
-}
-input {
-  background: rgba(255, 255, 255, 0.1);
-  border: none;
-  color: rgb(194, 194, 194);
-  font-size: 24px;
-  outline: none;
-}
-p,
-h2 {
-  margin: 10px;
-  padding: 10px;
-  overflow-wrap: break-word;
-  max-width: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import EncryptView from "../views/Encrypt.vue";
-import DecryptView from "../views/Decrypt.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,16 +8,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    },
-    {
-      path: "/encrypt",
-      name: "encrypt",
-      component: EncryptView,
-    },
-    {
-      path: "/decrypt",
-      name: "decrypt",
-      component: DecryptView,
     },
   ],
 });
